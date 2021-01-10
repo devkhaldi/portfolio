97065442ebf81ddb111a01787583f1916e9da6ec
devkhaldi <dev.khaldi@gmail.com>
2021-01-10 13:25:25 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <form className='form'>
+        <div className='form-group mt-1'>
+          <label>Enter first name</label>
+          <input type='text' className='form-control' />
+        </div>
+        <div className='form-group'>
+          <label>Enter first name</label>
+          <input type='text' className='form-control' />
+        </div>
+      </form>
+      <button className='btn btn-success'>Click me</button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/Hook.js
@@ -0,0 +1,7 @@
+import React, { useState } from 'react'
+
+const Hook = () => {
+  return <div className='container'></div>
+}
+
+export default Hook
