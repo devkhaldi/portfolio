3955ea01f68741d5b6079bf7acb5daa93be10e18
devkhaldi <dev.khaldi@gmail.com>
2021-01-21 23:17:45 +0100

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
 import React from 'react'
 import logo from './logo.svg'
 import './App.css'
+import Navbar from './components/Navbar'
 
 function App() {
-  return (
-    <div className='container'>
-      <form className='form'>
-        <div className='form-group mt-1'>
-          <label>Enter first name</label>
-          <input type='text' className='form-control' />
-        </div>
-        <div className='form-group'>
-          <label>Enter first name</label>
-          <input type='text' className='form-control' />
-        </div>
-      </form>
-      <button className='btn btn-success'>Click me</button>
-    </div>
-  )
+  return <Navbar />
 }
 
 export default App
