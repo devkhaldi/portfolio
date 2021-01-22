3de4e0922e5f7e072be37bff070125bed3d219eb
devkhaldi <dev.khaldi@gmail.com>
2021-01-22 22:32:30 +0100

    Navbar structure

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,38 +1,58 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <nav className='navbar navbar-expand-lg navbar-light bg-light'>
-      <div className='container-fluid'>
-        <a className='navbar-brand' href='#'>
-          Navbar
-        </a>
-        <button
-          className='navbar-toggler'
-          type='button'
-          data-bs-toggle='collapse'
-          data-bs-target='#navbarSupportedContent'
-          aria-controls='navbarSupportedContent'
-          aria-expanded='false'
-          aria-label='Toggle navigation'>
-          <span className='navbar-toggler-icon'></span>
-        </button>
-        <div className='collapse navbar-collapse' id='navbarSupportedContent'>
-          <ul className='navbar-nav me-auto mb-2 mb-lg-0 ml-auto'>
-            <li className='nav-item'>
-              <a className='nav-link active' aria-current='page' href='#'>
-                Home
-              </a>
-            </li>
-            <li className='nav-item'>
-              <a className='nav-link' href='#'>
-                Link
-              </a>
-            </li>
-          </ul>
+    <header>
+      <div className='navbar animated medium'>
+        <AiOutlineMenu onClick={() => setIsOpened(!isOpened)} />
+        <div className='logo-container'>
+          <img src={logo} alt='logo' />
+        </div>
+        <div className='nav-items'>
+          <NavLink exact to='/'>
+            Home
+          </NavLink>
+          <NavLink exact to='/tours'>
+            Tours
+          </NavLink>
+          <NavLink exact to='/hotels'>
+            Hotels
+          </NavLink>
+          <NavLink exact to='/flights'>
+            Flights
+          </NavLink>
+          <NavLink exact to='/gallery'>
+            Gallery
+          </NavLink>
+          <NavLink exact to='/contact'>
+            Contact
+          </NavLink>
         </div>
       </div>
-    </nav>
+      <div className={isOpened ? 'side-nav' : 'side-nav opened'}>
+        <div className='side-nav-items'>
+          <NavLink exact to='/'>
+            Home
+          </NavLink>
+          <NavLink exact to='/tours'>
+            Tours
+          </NavLink>
+          <NavLink exact to='/hotels'>
+            Hotels
+          </NavLink>
+          <NavLink exact to='/flights'>
+            Flights
+          </NavLink>
+          <NavLink exact to='/gallery'>
+            Gallery
+          </NavLink>
+          <NavLink exact to='/contact'>
+            Contact
+          </NavLink>
+        </div>
+      </div>
+    </header>
   )
 }
 
