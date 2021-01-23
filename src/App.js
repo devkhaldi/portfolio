6ccb6fbf3c3a63c93de243eb0635ac3336125a66
devkhaldi <dev.khaldi@gmail.com>
2021-01-23 23:31:22 +0100

    Router

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import logo from './logo.svg'
 import Navbar from './components/Navbar'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import Home from './home/Home'
+import Portfolio from './portfolio/Portfolio'
+import About from './about/About'
+import GetInTouch from './getInTouch/GetInTouch'
 import './styles/Navbar.css'
 
 function App() {
@@ -11,8 +14,9 @@ function App() {
       <Navbar />
       <Switch>
         <Route exact path='/' component={Home} />
-        <Route exact path='/' component={Home} />
-        <Route exact path='/' component={Home} />
+        <Route exact path='/portfolio' component={Portfolio} />
+        <Route exact path='/about' component={About} />
+        <Route exact path='/getintouch' component={Home} />
       </Switch>
     </BrowserRouter>
   )
