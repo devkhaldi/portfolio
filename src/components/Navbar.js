550470665c6782594d9a822b65fad22805ae6337
devkhaldi <dev.khaldi@gmail.com>
2021-01-29 22:41:12 +0100

    Toggle navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import { Link, NavLink } from 'react-router-dom'
+import { AiOutlineMenu } from 'react-icons/ai'
 import './Navbar.css'
+import { useState } from 'react'
 
 const Navbar = () => {
+  const [open, setopen] = useState(false)
   return (
     <React.Fragment>
       <nav>
+        <AiOutlineMenu onClick={() => setopen(!open)} />
         <div className='logo-container'>
           <Link exact to='/'>
             Khaldi
@@ -21,7 +25,6 @@ const Navbar = () => {
           <NavLink className='nav-item' exact to='/about'>
             About
           </NavLink>
-
           <div className='contact'>
             <Link className='btn' exact to='/contact'>
               Contact
@@ -29,7 +32,7 @@ const Navbar = () => {
           </div>
         </div>
       </nav>
-      <div className='side-nav-items'>
+      <div className={open ? 'side-nav-items' : 'side-nav-items open'}>
         <NavLink className='nav-item' exact to='/'>
           Home
         </NavLink>
