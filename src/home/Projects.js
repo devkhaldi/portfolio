ffe2ba728028204fcf93e90b1a6e8769cda38c7b
devkhaldi <dev.khaldi@gmail.com>
2021-01-30 23:51:24 +0100

    Projects

--- src/home/Projects.js
+++ src/home/Projects.js
@@ -1,7 +1,15 @@
 import React from 'react'
+import Project from './Project'
 
 const Projects = () => {
-  return <section className='projects'></section>
+  return (
+    <section className='projects'>
+      <div className='header'>
+        <h1>Featured Work</h1>
+      </div>
+      <div className='projects-container'></div>
+    </section>
+  )
 }
 
 export default Projects
