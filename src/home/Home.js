aac30cfd45ed488d5fcc350cd81b0cad33976641
devkhaldi <dev.khaldi@gmail.com>
2021-01-30 23:27:53 +0100

    Init Home

--- src/home/Home.js
+++ src/home/Home.js
@@ -1,8 +1,17 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import Hero from './Hero'
+import Projects from './Projects'
+import Footer from '../components/Footer'
 
 const Home = () => {
-  return <React.Fragment></React.Fragment>
+  return (
+    <React.Fragment>
+      <Hero />
+      <Projects />
+      <Footer />
+    </React.Fragment>
+  )
 }
 
 export default Home
