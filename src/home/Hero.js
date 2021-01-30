29bc1bcb59180613a8ccd4b3d08d9c0a6e3cf8dc
devkhaldi <dev.khaldi@gmail.com>
2021-01-30 23:46:06 +0100

    Home Hero

--- src/home/Hero.js
+++ src/home/Hero.js
@@ -1,7 +1,29 @@
 import React from 'react'
 
 const Hero = () => {
-  return <section className='hero'>Hero</section>
+  return (
+    <section className='hero'>
+      <h1>Successful Mern stack apps Development</h1>
+      <p>Hi. I’m Khaldi, a Full stack Developer creating successful websites. Services</p>
+      <div className='cards'>
+        <div className='card'>
+          <img src='' alt='' />
+          <h2>UI,UX design implementation</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, quas.</p>
+        </div>
+        <div className='card'>
+          <img src='' alt='' />
+          <h2>React apps developement</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, quas.</p>
+        </div>
+        <div className='card'>
+          <img src='' alt='' />
+          <h2>APIs Building using Express</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, quas.</p>
+        </div>
+      </div>
+    </section>
+  )
 }
 
 export default Hero
