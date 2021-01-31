19f52a2c875c773d3e9207f287c3050b4b8221ac
devkhaldi <dev.khaldi@gmail.com>
2021-01-31 21:07:03 +0100

    Project

--- src/home/Project.js
+++ src/home/Project.js
@@ -1,7 +1,18 @@
 import React from 'react'
 
-const Project = () => {
-  return <div className='project'></div>
+const Project = ({ miniImage, title, description, mainImage }) => {
+  return (
+    <div className='project'>
+      <div className='col-md-4'>
+        <img src={miniImage} alt={`${title} + ${miniImage}`} />
+        <h3>{title}</h3>
+        <p>{description}</p>
+      </div>
+      <div className='col-md-8'>
+        <img src={mainImage} alt={`${title} + ${mainImage}`} />
+      </div>
+    </div>
+  )
 }
 
 export default Project
