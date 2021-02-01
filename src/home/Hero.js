a9d9e8e5c1390d30b1dee7654d7b506c87bb4208
devkhaldi <dev.khaldi@gmail.com>
2021-02-01 15:54:40 +0100

    Hero cards style

--- src/home/Hero.js
+++ src/home/Hero.js
@@ -12,33 +12,21 @@ const Hero = () => {
       </div>
 
       <div className='hero-cards container'>
-        <div className='row flex-md-nowrap'>
-          <div className='card col-md-4 mx-md-3'>
-            <img
-              className='w-50'
-              src='https://ianlunn.co.uk/static/0fe210187ee16bce95bb97c7ab3db41b/98c70/feature__web.webp'
-              alt=''
-            />
-            <h2>UI,UX design implementation</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, quas.</p>
+        <div className='cards'>
+          <div className='card mx-md-3'>
+            <img src='https://ianlunn.co.uk/static/0fe210187ee16bce95bb97c7ab3db41b/98c70/feature__web.webp' alt='' />
+            <h3>UI,UX design implementation</h3>
+            <p>Responsive websites built for an optimal user experience that achieves your business goals.</p>
           </div>
-          <div className='card col-md-4 mx-md-3'>
-            <img
-              className='w-50'
-              src='https://ianlunn.co.uk/static/54f14637ec376524313424344767614c/98c70/feature__tools.webp'
-              alt=''
-            />
-            <h2>React apps developement</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, quas.</p>
+          <div className='card mx-md-3'>
+            <img src='https://ianlunn.co.uk/static/54f14637ec376524313424344767614c/98c70/feature__tools.webp' alt='' />
+            <h3>React apps developement</h3>
+            <p>Make your website fast, easy to find, and reach the widest audience possible.</p>
           </div>
-          <div className='card col-md-4 mx-md-3'>
-            <img
-              className='w-50'
-              src='https://ianlunn.co.uk/static/fcaac44c20804fb66461aed9d410badd/98c70/feature__wp.webp'
-              alt=''
-            />
-            <h2>APIs Building using Express</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, quas.</p>
+          <div className='card mx-md-3'>
+            <img src='https://ianlunn.co.uk/static/fcaac44c20804fb66461aed9d410badd/98c70/feature__wp.webp' alt='' />
+            <h3>APIs Building using Express</h3>
+            <p>Manage your website using the web's most popular content management system.</p>
           </div>
         </div>
       </div>
