c8eb69fa77c4090789b9f3df563b42dffb7dcae8
devkhaldi <dev.khaldi@gmail.com>
2021-02-01 14:48:58 +0100

    Style Hero text

--- src/home/Hero.js
+++ src/home/Hero.js
@@ -1,13 +1,19 @@
 import React from 'react'
+import './Hero.css'
 
 const Hero = () => {
   return (
     <section className='hero'>
-      <h1>Successful Mern stack apps Development</h1>
-      <p>Hi. I’m Khaldi, a Full stack Developer creating successful websites. Services</p>
-      <div className='container'>
+      <div className='hero-text'>
+        <h1>Successful Web-apps Development</h1>
+        <p>
+          Hi. I’m Ian, a freelance Front-end Developer with 12 years commercial experience creating successful websites.
+        </p>
+      </div>
+
+      <div className='hero-cards container'>
         <div className='row flex-md-nowrap'>
-          <div className='card col-md-4 mx-md-2'>
+          <div className='card col-md-4 mx-md-3'>
             <img
               className='w-50'
               src='https://ianlunn.co.uk/static/0fe210187ee16bce95bb97c7ab3db41b/98c70/feature__web.webp'
@@ -16,7 +22,7 @@ const Hero = () => {
             <h2>UI,UX design implementation</h2>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, quas.</p>
           </div>
-          <div className='card col-md-4 mx-md-2'>
+          <div className='card col-md-4 mx-md-3'>
             <img
               className='w-50'
               src='https://ianlunn.co.uk/static/54f14637ec376524313424344767614c/98c70/feature__tools.webp'
@@ -25,7 +31,7 @@ const Hero = () => {
             <h2>React apps developement</h2>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, quas.</p>
           </div>
-          <div className='card col-md-4 mx-md-2'>
+          <div className='card col-md-4 mx-md-3'>
             <img
               className='w-50'
               src='https://ianlunn.co.uk/static/fcaac44c20804fb66461aed9d410badd/98c70/feature__wp.webp'
