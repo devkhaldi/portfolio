126ca0cb09abb649e7abaefbd005cf629a13b3e4
devkhaldi <dev.khaldi@gmail.com>
2021-02-02 17:10:58 +0100

    portfolio hero

--- src/portfolio/Hero.js
+++ src/portfolio/Hero.js
@@ -1,7 +1,18 @@
 import React from 'react'
+import './Hero.css'
 
 const Hero = () => {
-  return <section className='hero'></section>
+  return (
+    <section className='portfolio-hero'>
+      <div className='hero-text'>
+        <h1>Front-end Development Portfolio</h1>
+        <p>
+          Succeed online with a website that is fast, easy to use, and built with best practices and a passion for
+          design.
+        </p>
+      </div>
+    </section>
+  )
 }
 
 export default Hero
