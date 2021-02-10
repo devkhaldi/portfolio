25499e37bc8204da0e78fe6d898056ca0e11d643
devkhaldi <dev.khaldi@gmail.com>
2021-02-10 23:33:19 +0100

    Open modal

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import './Project.css'
+import ProjectModal from './ProjectModal'
 
 const Project = ({ miniImage, title, description, mainImage }) => {
   return (
     <div className='project'>
+      <ProjectModal />
       <div className='project-info col-lg-4 mx-md-2'>
-        <img src={miniImage} alt={`${title} + ${miniImage}`} />
-        <h3>{title}</h3>
+        <img src={miniImage} alt={`${title} + ${miniImage}`} data-toggle='modal' data-target='#modal-default' />
+        <h3 data-toggle='modal' data-target='#modal-default'>
+          {title}
+        </h3>
         <p>{description}</p>
       </div>
       <div className='project-preview col-lg-8 mx-md-2'>
