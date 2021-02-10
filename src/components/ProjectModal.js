0b43c1e2caedd21e88ca5a285528a8645c42eeaa
devkhaldi <dev.khaldi@gmail.com>
2021-02-10 23:29:07 +0100

    Project Modal

--- src/components/ProjectModal.js
+++ src/components/ProjectModal.js
@@ -1,7 +1,12 @@
 import React from 'react'
+import Modal from './Modal'
 
 const ProjectModal = () => {
-  return <div></div>
+  return (
+    <Modal title='Project'>
+      <h1>Project</h1>
+    </Modal>
+  )
 }
 
 export default ProjectModal
