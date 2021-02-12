7a4c0fd7b63d1083ec941db95b4e888a10b82b76
devkhaldi <dev.khaldi@gmail.com>
2021-02-12 20:16:37 +0100

    Init Project Modal implementation

--- src/components/ProjectModal.js
+++ src/components/ProjectModal.js
@@ -5,6 +5,8 @@ const ProjectModal = ({ project }) => {
   return (
     <Modal title='Project'>
       <h1>{project.title}</h1>
+      <p>{project.description}</p>
+      <img src={project.mainImage} className='w-100' alt={`${project.mainImage} ${project.title}`} />
     </Modal>
   )
 }
