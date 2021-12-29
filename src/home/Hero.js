db8ea6b29db81a926f714899c81810b01d4ea5ea
devkhaldi <dev.khaldi@gmail.com>
2021-12-29 19:42:34 +0000

    hero illustration

--- src/home/Hero.js
+++ src/home/Hero.js
@@ -1,24 +1,37 @@
-import React from 'react'
-import './Hero.css'
+import React from "react"
+import "./Hero.css"
+import illustration from "../img/undraw_programming.svg"
 
 const Hero = () => {
   return (
     <section className='hero'>
       <div className='hero-text'>
-        <h1>Successful Web applications Development</h1>
-        <p>
-          Hi. I’m Ian, a freelance Front-end Developer with 12 years commercial experience creating successful websites.
-        </p>
+        <div className='text-content'>
+          <h1>Successful Web applications Development</h1>
+          <p>
+            Hi. I’m Ian, a freelance Front-end Developer with 12 years
+            commercial experience creating successful websites.
+          </p>
+        </div>
+        <div className='hero-illustration'>
+          <img src={illustration} alt='illustration' />
+        </div>
       </div>
       <div className='hero-cards container'>
         <div className='cards'>
           <div className='card mx-md-3'>
             <div className='img-container'>
-              <img src='https://ianlunn.co.uk/static/0fe210187ee16bce95bb97c7ab3db41b/98c70/feature__web.webp' alt='' />
+              <img
+                src='https://ianlunn.co.uk/static/0fe210187ee16bce95bb97c7ab3db41b/98c70/feature__web.webp'
+                alt=''
+              />
             </div>
             <div className='text-container'>
               <h3>UI,UX design implementation</h3>
-              <p>Responsive websites built for an optimal user experience that achieves your business goals.</p>
+              <p>
+                Responsive websites built for an optimal user experience that
+                achieves your business goals.
+              </p>
             </div>
           </div>
           <div className='card mx-md-3'>
@@ -30,16 +43,25 @@ const Hero = () => {
             </div>
             <div className='text-container'>
               <h3>React apps developement</h3>
-              <p>Make your website fast, easy to find, and reach the widest audience possible.</p>
+              <p>
+                Make your website fast, easy to find, and reach the widest
+                audience possible.
+              </p>
             </div>
           </div>
           <div className='card mx-md-3'>
             <div className='img-container'>
-              <img src='https://ianlunn.co.uk/static/fcaac44c20804fb66461aed9d410badd/98c70/feature__wp.webp' alt='' />
+              <img
+                src='https://ianlunn.co.uk/static/fcaac44c20804fb66461aed9d410badd/98c70/feature__wp.webp'
+                alt=''
+              />
             </div>
             <div className='text-container'>
               <h3>APIs Building using Express</h3>
-              <p>Manage your website using the web's most popular content management system.</p>
+              <p>
+                Manage your website using the web's most popular content
+                management system.
+              </p>
             </div>
           </div>
         </div>
--- src/portfolio/Hero.js
+++ src/portfolio/Hero.js
@@ -1,5 +1,6 @@
-import React from 'react'
-import './Hero.css'
+import React from "react"
+import illustration from "../img/undraw_programming.svg"
+import "./Hero.css"
 
 const Hero = () => {
   return (
@@ -7,8 +8,8 @@ const Hero = () => {
       <div className='hero-text'>
         <h1>Front-end Development Portfolio</h1>
         <p>
-          Succeed online with a website that is fast, easy to use, and built with best practices and a passion for
-          design.
+          Succeed online with a website that is fast, easy to use, and built
+          with best practices and a passion for design.
         </p>
       </div>
     </section>
