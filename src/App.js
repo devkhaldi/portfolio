a0523e64b960a4054f2c4107d8f84b4230e8616e
devkhaldi <dev.khaldi@gmail.com>
2022-01-24 05:27:53 +0000

    fix image and redirects

--- src/App.js
+++ src/App.js
@@ -16,10 +16,10 @@ function App() {
     <BrowserRouter>
       <Navbar />
       <Switch>
-        <Route exact path='/' component={Home} />
         <Route exact path='/portfolio' component={Portfolio} />
         <Route exact path='/about' component={About} />
         <Route exact path='/contact' component={Contact} />
+        <Route path='/' component={Home} />
       </Switch>
       <Footer />
     </BrowserRouter>
