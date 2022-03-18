d238a3e747fbd3b7bda24d4f995a70a9bd32fa62
devkhaldi <dev.khaldi@gmail.com>
2022-03-18 04:23:15 +0100

    footer ellipses

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,44 @@
-import React from "react"
+import React, { useEffect } from "react"
 import { FaLinkedinIn, FaGithub } from "react-icons/fa"
 import { Link } from "react-router-dom"
 import "./Footer.css"
 import myPhoto from "../img/myphoto.jpg"
+import ellipse1 from "../img/ellipse1.svg"
 
 const Footer = () => {
+  const rotateEllipse = () => {
+    const ellipse1 = document.getElementById("footer-ellipse1")
+    const ellipse2 = document.getElementById("footer-ellipse2")
+    // const ellipse3 = document.getElementById("ellipse3")
+    let rotationAngle = 0
+
+    setInterval(() => {
+      console.log("rotate")
+      ellipse1.style.transform = `rotateZ(${rotationAngle}deg)`
+      ellipse2.style.transform = `rotateZ(${-rotationAngle}deg)`
+      // ellipse2.style.transform = `rotateZ(${0.5 * rotationAngle}deg)`
+      rotationAngle++
+    }, 300)
+  }
+
+  useEffect(() => {
+    rotateEllipse()
+  }, [])
   return (
     <footer>
+      <img
+        src={ellipse1}
+        className='ellipse1'
+        id='footer-ellipse1'
+        alt='ellipse1'
+      />
+      <img
+        src={ellipse1}
+        className='ellipse2'
+        id='footer-ellipse2'
+        alt='footer-ellipse2'
+      />
+      {/* <img src={ellipse1} className='ellipse3' id='ellipse3' alt='ellipse3' /> */}
       <div className='footer-content  my-container'>
         <div className='about'>
           <div className='img-container'>
