260cf5bc52fff1db7581c74d9d5e7d698b0ff7db
elkhaldi <dev.khaldi@gmail.com>
2023-04-28 23:29:58 +0100

    home animation to top

--- src/home/Home.js
+++ src/home/Home.js
@@ -1,15 +1,15 @@
-import React from "react"
+import React, { useEffect } from "react"
 import Hero from "./Hero"
 import Projects from "./Projects"
 
 const Home = () => {
-  // useEffect(() => {
-  //   window.scroll({
-  //     top: 0,
-  //     left: 0,
-  //     behavior: "smooth",
-  //   })
-  // }, [])
+  useEffect(() => {
+    window.scroll({
+      top: 0,
+      left: 0,
+      behavior: "smooth",
+    })
+  }, [])
   return (
     <React.Fragment>
       <Hero />
